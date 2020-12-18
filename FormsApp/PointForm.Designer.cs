de50934d5bee8a6e3a04a58acb84aeee598ff7ce
Nikita <68366435+Scanavik@users.noreply.github.com>
2020-12-18 07:32:40 +0500

    Add files via upload

---
+++ FormsApp/PointForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace FormsApp
+{
+    partial class PointForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnSerialize = new System.Windows.Forms.Button();
+            this.btnDeserialize = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(55, 355);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(99, 34);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(175, 355);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(99, 34);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnSerialize
+            // 
+            this.btnSerialize.Location = new System.Drawing.Point(295, 355);
+            this.btnSerialize.Name = "btnSerialize";
+            this.btnSerialize.Size = new System.Drawing.Size(99, 34);
+            this.btnSerialize.TabIndex = 2;
+            this.btnSerialize.Text = "Serialize";
+            this.btnSerialize.UseVisualStyleBackColor = true;
+            this.btnSerialize.Click += new System.EventHandler(this.btnSerialize_Click);
+            // 
+            // btnDeserialize
+            // 
+            this.btnDeserialize.Location = new System.Drawing.Point(414, 355);
+            this.btnDeserialize.Name = "btnDeserialize";
+            this.btnDeserialize.Size = new System.Drawing.Size(99, 34);
+            this.btnDeserialize.TabIndex = 3;
+            this.btnDeserialize.Text = "Deserialize";
+            this.btnDeserialize.UseVisualStyleBackColor = true;
+            this.btnDeserialize.Click += new System.EventHandler(this.btnDeserialize_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(55, 23);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(458, 290);
+            this.listBox.TabIndex = 4;
+            // 
+            // PointForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnDeserialize);
+            this.Controls.Add(this.btnSerialize);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "PointForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnSerialize;
+        private System.Windows.Forms.Button btnDeserialize;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
